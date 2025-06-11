9d0f35298d6d458584e7baaf6aa23f4173f1f9da
Sonu Kumar <156998478+Sonukumar3552@users.noreply.github.com>
2025-06-11 21:48:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,137 @@
+// // var age=25;
+
+// // if(true){
+// //     console.log(age);
+// // }
+// function solve(){
+//     var age=25;
+//     console.log(age);
+// }
+// solve();
+// let a=10;
+// console.log(a);
+
+// const a=20;
+// console.log(a);
+
+// let a=10;
+// let b=5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(5>30);
+// console.log(2==3);
+
+// let age=5;
+// let stetus=(age>18)?'i can vote':'i can not vote';
+// console.log(stetus);
+
+// age=53;
+// if (age>=18) {
+//     console.log("vote");
+// } else {
+//     console.log("cant vote");
+// }
+
+// let num=5;
+
+// if (num==1) {
+//     console.log("a");
+// }
+// else if(num==2){
+//     console.log("b");
+// }
+// else if(num==3){
+//     console.log("c");
+// }
+// else if(num==4){
+//     console.log("d");
+// }
+// else if(num==5){
+//     console.log("e");
+// }
+// else if(num==6){
+//     console.log("f");
+// }
+// else if(num==7){
+//     console.log("g");
+// }
+// else{
+//     console.log("h")
+// }
+
+// let num=3;
+
+// switch (num) {
+//     case 1:
+//         console.log("a")
+//         break;
+//     case 2:
+//         console.log("b")
+//         break;
+//     case 3:
+//         console.log("c")
+//         break;
+//     case 4:
+//         console.log("d")
+//         break;
+//     default:
+//         break;
+// }
+
+// for(i=1; i<=10; i++)
+// {
+//     console.log("sonu")
+// }
+
+// i=1;
+// while(i<=10)
+// {
+//     console.log("sonu");
+//     i++;
+// }
+
+// i=10;
+// do{
+    
+//     console.log("sonu");
+//       i++;
+// }
+// while(i<=10)
+  
+// let i=10;
+// do{
+//     console.log(i)
+//     i++;
+// }
+// while(i<50);
+
+// let firstName="sonu";
+// let lastName='maahi';
+// console.log(firstName);
+// let ans=firstName+lastName;
+// console.log(ans);
+// console.log(firstName.substring(1,4));
+// console.log(object);
+
+// function sayMyname(){
+//     console.log("sonu maahi")
+// }
+// sayMyname();
+
+function printCounting()
+{
+    for(let i=1;i<100;i++){
+        console.log(i);
+    }
+}
+printCounting();
+
+function printnum(num) {
+    console.log(num);
+}
+printnum(50);
