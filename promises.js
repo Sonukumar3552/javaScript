a5c4d911bc16a61269b3743dbfc8e1d8836f1d95
Sonu Kumar <156998478+Sonukumar3552@users.noreply.github.com>
2025-06-16 16:04:58 +0530

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,12 @@
+
+
+// let anotherPromise = new Promise( (resolve, reject)=>{
+//     console.log("sonu Maahi");
+//     resolve(1001);
+// });
+
+function sayMyname(){
+    console.log("My name is sonu kumar");
+
+}
+setTimeout(sayMyname,10000);
